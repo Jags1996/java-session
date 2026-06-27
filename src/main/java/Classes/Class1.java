19faa21e5e1d723aadd3319809562c168b4776aa
Jagdish Badve <jagdish.badve@gmail.com>
2026-06-27 23:46:44 +0530

    automation

---
+++ src/main/java/Classes/Class1.java
@@ -0,0 +1,27 @@
+package Classes;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+
+		String day = "tues";
+		switch (day) {
+		case "monday":
+			System.out.println("in");
+			break;
+		case "Tues":
+			System.out.println("in");
+			break;
+		case "wend":
+			System.out.println("in");
+			break;
+		case "thurs":
+			System.out.println("in");
+			break;
+
+		default:
+			System.out.println("out");
+		}
+
+	}
+}
